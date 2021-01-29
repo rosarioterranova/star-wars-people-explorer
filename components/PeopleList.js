c536f223af211455ba2bbbdaba3dd745f0231b9e
Rosario Terranova <rosarioterranova91@gmail.com>
2021-01-29 18:37:46 +0100

    refactoring

--- components/PeopleList.js
+++ components/PeopleList.js
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function PeopleList({people}) {
   return (
-      <ul className="list-group">
+      <ul className="list-group my-3">
           {
-            people.map((person,index) => <li className="list-group-item"><Link href={`/person/${index}`}>{person.name}</Link></li>)
+            people.map((person,index) => <li key={index} className="list-group-item"><Link href={`/person/${index}`}>{person.name}</Link></li>)
           }
       </ul>
   )
--- pages/person/[id]/index.js
+++ pages/person/[id]/index.js
@@ -14,31 +14,30 @@ export default function Person({data}) {
         router.push(`/person/${Number(id)-1}`)
     }
 
+    let body = (<p>Loading...</p>)
+    if(data[id] !== undefined){
+        body = (
+            <>
+                <Header title={`${data[id].name} | SWPE`} />
+                <h1>{data[id].name}</h1>
+                <ul>
+                    <li>Height: {data[id].height}</li>
+                    <li>mass: {data[id].mass}</li>
+                    <li>Hair Color: {data[id].hair_color}</li>
+                    <li>Skin Color: {data[id].skin_color}</li>
+                    <li>Eye Csolor: {data[id].eye_color}</li>
+                    <li>Birth Year: {data[id].birth_year}</li>
+                    <li>Gender: {data[id].gender}</li>
+                </ul>
+            </>
+        )
+    }
+
     return (
         <div className="index container">
-            {
-                data[id] !== undefined ? 
-                (<>
-                    <Header title={`${data[id].name} | SWPE`} />
-                    <h1>{data[id].name}</h1>
-                    <ul>
-                        <li>Height: {data[id].height}</li>
-                        <li>mass: {data[id].mass}</li>
-                        <li>Hair Color: {data[id].hair_color}</li>
-                        <li>Skin Color: {data[id].skin_color}</li>
-                        <li>Eye Csolor: {data[id].eye_color}</li>
-                        <li>Birth Year: {data[id].birth_year}</li>
-                        <li>Gender: {data[id].gender}</li>
-                    </ul>
-                </>)
-
-                : (<>
-                <Header title="Loading | SWPE" />
-                <p>Loading...</p>
-                </>)
-            }
-        <button className="btn btn-dark m-1" onClick={goPreviuos} disabled={id == 0 ? true:false}>Previous</button>
-        <button className="btn btn-dark m-1" onClick={goNext} disabled={id == data.length-1 ? true:false}>Next</button>
+            {body}
+            <button className="btn btn-dark m-1" onClick={goPreviuos} disabled={id == 0 ? true:false}>Previous</button>
+            <button className="btn btn-dark m-1" onClick={goNext} disabled={id == data.length-1 ? true:false}>Next</button>
         </div>
     )
 }
